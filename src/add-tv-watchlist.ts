4f3efe9fa44c05ff4446ad52ac411fabf4ed2dbe
svsudharshan <75555000+svsudharshan@users.noreply.github.com>
2020-12-06 09:45:25 +0530

    Add files via upload

--- src/add-tv-watchlist.ts
+++ src/add-tv-watchlist.ts
@@ -38,7 +38,7 @@ const fetchFirstXPath = async (selector: string, page, timeout = 20000) => {
 const addAlert = async (symbol: string, alertConfig: any, page) => {
 
 
-    const {indicator, signal, option, message } = alertConfig
+    const {} = alertConfig
 
     //await page.waitForXPath('//*[@id="header-toolbar-symbol-search"]/div/input')
 
@@ -49,67 +49,10 @@ const addAlert = async (symbol: string, alertConfig: any, page) => {
     await delay(500);
     const symbolInput = await fetchFirstXPath('//input[@data-role=\'search\']', page)
     await symbolInput.type(`  ${symbol}${String.fromCharCode(13)}`)
-
-    await delay(9000);
-
-    const alertButton = await fetchFirstXPath('//*[@id="header-toolbar-alerts"]', page)
-
-    await alertButton.click()
-
-
-    await delay(1000);
-    const conditionDropDown = await fetchFirstXPath("//*[@class='tv-alert-dialog__group-item tv-alert-dialog__group-item--left js-main-series-select-wrap']/*[@class='tv-control-select__wrap tv-dropdown-behavior tv-control-select--size_small' and 1]/*[@class='tv-control-select__control tv-dropdown-behavior__button' and 1]", page)
-
-    conditionDropDown.click()
-    await delay(1000);
-
-    const optionMTF = await fetchFirstXPath(`//*[@class='tv-control-select__option-wrap' and contains(text(), '${indicator}')]`, page)
-    optionMTF.click()
-
-    await delay(1000);
-
-    const signalDropDown = await fetchFirstXPath("//*[@class='tv-control-fieldset__value tv-alert-dialog__fieldset-value js-condition-operator-input-wrap']/span[@class='tv-control-select__wrap tv-dropdown-behavior tv-control-select--size_small' and 1]/span[@class='tv-control-select__control tv-dropdown-behavior__button' and 1]", page)
-    signalDropDown.click()
-
-    await delay(1000);
-    const optionSignal = await fetchFirstXPath(`//*[@class='tv-control-select__option-wrap' and contains(text(), '${signal}')]`, page)
-    optionSignal.click()
-
-    await delay(1000);
-    const freqButton = await fetchFirstXPath(`//*[text()='${option}']`, page)
-    freqButton.click()
-
-    await delay(1000);
-
-    
-    //await page.evaluate("textarea[name=description]", el => el.value = "")
-
-    const messageTextarea = await fetchFirstXPath("//textarea[@class='tv-control-textarea']", page)
-
-    messageTextarea.click({clickCount: 3})
-
-    await delay(500);
-    await messageTextarea.press('Backspace');
-    await delay(500);
-
-    const messageText = `${message}` //message.toString().replace(/{{quote}}/g, quote).replace().replace(/{{base}}/g, base).replace()
-
-    await messageTextarea.type(messageText)
-
-    await delay(1000);
-    const continueButton = await fetchFirstXPath("//span[@class='tv-button__loader']", page)
-    continueButton.click()
-
-    await delay(1500);
-
-    try {
-        const continueAnywayButton = await fetchFirstXPath("//*[text()='Continue anyway']", page,3000)
-        continueAnywayButton.click()
-        await delay(2000);
-    } catch (error) {
-        //must not be alert on an indicator
-    }
-
+    await delay(2000);
+	await page.keyboard.down('Alt');
+	await page.keyboard.press('KeyW');
+	await page.keyboard.up('Alt');
 
 }
 
@@ -163,11 +106,9 @@ const main = async () => {
 		const symbolRows = await readFilePromise(config.files.input)
 
         for (const row of symbolRows) {
-			await page.keyboard.type(`${config.tradingview.timeframe}${String.fromCharCode(13)}`);
-			await delay(200)
 
             console.log(`Adding symbol: ${row.symbol}`)
-            await delay(5000)
+            await delay(500)
             await addAlert(row.symbol, alertConfig, page)
         }
     }
